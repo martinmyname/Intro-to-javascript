093c9141301d8025e9a3af1853bad8e7e95b8c58
martin <kaweesimartinanderson@gmail.com>
2019-06-14 01:56:24 -0400

    cardnumber

---
+++ null_checker.js
@@ -0,0 +1,16 @@
+
+for (count=1;count<=10;count++) 
+	{
+let cardNumber= prompt("What is your bag card number?");
+if (cardNumber>0)
+	 {console.log(1);}
+
+else if (cardNumber<0)
+	{
+	console.log(-1);
+	}
+else if (cardNumber==0)
+	{
+	console.log(0);
+	
+	}}
